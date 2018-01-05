6de000b797f1830ff795f76005551a05d93f1864
Houssem Dellai <houssem.dellai.ext@geodis.com>
2018-01-05 01:16:22 +0100

    Add project files.

---
+++ Repeater/Repeater/ViewModels/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Repeater.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private List<string> _employees;
+        private List<string> _students;
+
+        public List<string> Employees
+        {
+            get => _employees;
+            set
+            {
+                _employees = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public List<string> Students
+        {
+            get => _students;
+            set
+            {
+                _students = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SelectStudentCommand => new Command(student =>
+        {
+
+        });
+        
+        public MainViewModel()
+        {
+            Employees = new List<string>
+            {
+                "Adam", "Hassen", "Gabriel", "Joseph", "Marie"
+            };
+
+            Students = new List<string>
+            {
+                "Omar", "Seif", "Mahdi", "Safa", "Kamel"
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Repeater/Repeater/Views/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Repeater.ViewModels;
+using Xamarin.Forms;
+
+namespace Repeater.Views
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private void Employee_Tapped(object sender, EventArgs e)
+	    {
+	        var view = sender as View;
+
+	        var employee = view?.BindingContext as string;
+
+	        DisplayAlert("Tap Event", "You have selected " + employee, "Ok");
+	    }
+
+	    private void Student_Tapped(object sender, EventArgs e)
+	    {
+	        var view = sender as View;
+
+	        var student = view?.BindingContext as string;
+
+	        var vm = BindingContext as MainViewModel;
+
+            vm.SelectStudentCommand.Execute(student);
+	    }
+	}
+}
